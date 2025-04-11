06f5666c06eb635233ff2da099e8d01768753eb7
rinoagung <rinoagung55@gmail.com>
2025-04-11 15:01:01 +0700

    crud for notes image

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,4 +1,6 @@
 const runQuery = require('../config/db');
+const path = require('path');
+const fs = require('fs');
 
 exports.getAllNotes = async (req, res) => {
     try {
@@ -23,7 +25,15 @@ exports.getNoteById = async (req, res) => {
 exports.createNote = async (req, res) => {
     try {
         const { title, content } = req.body;
-        const image = req.file ? `/uploads/${req.file.filename}` : null;
+
+        if (!title || !content) {
+            return res.status(400).json({
+                success: false,
+                message: 'Judul dan isi catatan tidak boleh kosong'
+            });
+        }
+
+        const image = req.file ? `${req.file.filename}` : null;
 
         const result = await runQuery(
             `INSERT INTO notes (title, content, image) VALUES ($1, $2, $3) RETURNING *`,
@@ -40,15 +50,34 @@ exports.updateNote = async (req, res) => {
     try {
         const { id } = req.params;
         const { title, content } = req.body;
+        if (!title || !content) {
+            return res.status(400).json({
+                success: false,
+                message: 'Judul dan isi catatan tidak boleh kosong'
+            });
+        }
+        let newImageName = null;
 
         const oldNote = await runQuery('SELECT * FROM notes WHERE id = $1', [id]);
         if (oldNote.rows.length === 0) return res.status(404).json({ message: 'Note not found' });
 
-        const image = req.file ? `/uploads/${req.file.filename}` : oldNote.rows[0].image;
+        // Jika ada file gambar baru dikirimkan
+        if (req.file) {
+            const filePath = path.join(__dirname, '..\\uploads\\notes', oldNote.rows[0].image);
+            const oldImagePath = oldNote.rows[0].image ? filePath : null;
+
+            // Hapus gambar lama (jika ada)
+            if (oldImagePath && fs.existsSync(oldImagePath)) {
+                fs.unlinkSync(oldImagePath);
+            }
+
+            // Simpan path gambar baru
+            newImageName = req.file.filename;
+        }
 
         const result = await runQuery(
             `UPDATE notes SET title = $1, content = $2, image = $3, updated_at = CURRENT_TIMESTAMP WHERE id = $4 RETURNING *`,
-            [title || oldNote.rows[0].title, content || oldNote.rows[0].content, image, id]
+            [title || oldNote.rows[0].title, content || oldNote.rows[0].content, newImageName, id]
         );
 
         res.json({ success: true, message: "Data berhasil diupdate", data: result.rows[0] });
@@ -61,10 +90,46 @@ exports.deleteNote = async (req, res) => {
     try {
         const { id } = req.params;
 
-        const result = await runQuery('DELETE FROM notes WHERE id = $1 RETURNING *', [id]);
-        if (result.rows.length === 0) return res.status(404).json({ message: 'Note not found' });
+        const select = await runQuery('SELECT * FROM notes WHERE id = $1', [id]);
+        if (select.rows.length === 0) return res.status(404).json({ message: 'Note not found' });
 
-        res.json({ success: true, message: "Data berhasil dihapus", deleted: result.rows[0] });
+        const note = select.rows[0];
+
+        const result = await runQuery('DELETE FROM notes WHERE id = $1 RETURNING *', [id]);
+
+        // Hapus file gambar jika ada
+        if (note.image) {
+            const imagePath = path.join(__dirname, '..', 'uploads\\notes\\', path.basename(note.image));
+            fs.unlink(imagePath, (err) => {
+                if (err) console.error('Gagal menghapus gambar:', err.message);
+            });
+        }
+
+        res.json({ success: true, message: 'Data berhasil dihapus', deleted: result.rows[0] });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+exports.getImage = async (req, res) => {
+    try {
+        const filePath = path.join(__dirname, '..\\uploads\\notes', req.params.filename);
+        res.sendFile(filePath);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
+
+exports.deleteImage = async (req, res) => {
+    try {
+        const filePath = path.join(__dirname, '..\\uploads\\notes', req.params.filename);
+
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                return res.status(404).json({ success: false, message: 'File tidak ditemukan atau gagal dihapus' });
+            }
+            res.json({ success: true, message: 'Gambar berhasil dihapus' });
+        });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -5,10 +5,19 @@ const multer = require('multer');
 
 // multer config
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'),
+    destination: (req, file, cb) => cb(null, 'uploads/notes/'),
     filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
-const upload = multer({ storage });
+const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = ['image/jpeg', 'image/png'];
+        if (!allowedTypes.includes(file.mimetype)) {
+            return cb(new Error('Only JPG and PNG allowed'), false);
+        }
+        cb(null, true);
+    }
+});
 
 // Routes
 router.get('/', notesController.getAllNotes);
@@ -16,5 +25,7 @@ router.get('/:id', notesController.getNoteById);
 router.post('/', upload.single('image'), notesController.createNote);
 router.put('/:id', upload.single('image'), notesController.updateNote);
 router.delete('/:id', notesController.deleteNote);
+router.get('/uploads/:filename', notesController.getImage);
+router.delete('/delete/:filename', notesController.deleteImage);
 
 module.exports = router;
