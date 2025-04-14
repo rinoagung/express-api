515c3625cde2560adc9d9796d595869e72a82e7d
rinoagung <rinoagung55@gmail.com>
2025-04-14 13:19:05 +0700

    change db connection

--- config/db.js
+++ config/db.js
@@ -2,17 +2,17 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 // local
-const dbConfig = {
-    user: process.env.POSTGRES_USERNAME,
-    host: process.env.POSTGRES_HOST,
-    database: process.env.POSTGRES_DATABASE,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-};
-
 // const dbConfig = {
-//     connectionString: process.env.POSTGRES_URL,
+//     user: process.env.POSTGRES_USERNAME,
+//     host: process.env.POSTGRES_HOST,
+//     database: process.env.POSTGRES_DATABASE,
+//     password: process.env.POSTGRES_PASSWORD,
+//     port: process.env.POSTGRES_PORT,
 // };
+
+const dbConfig = {
+    connectionString: process.env.POSTGRES_URL,
+};
 
 const runQuery = async (query, values = []) => {
     const client = new Client(dbConfig);
